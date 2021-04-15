591342e1a1829098c5c664d4461d6cd8db56948b
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2021-04-16 01:32:26 +0200

    Show your location app - added google map

--- AndroidProjects/ShowYourLocation/app/src/main/java/com/kpuc/showyourlocation/MainActivity.kt
+++ AndroidProjects/ShowYourLocation/app/src/main/java/com/kpuc/showyourlocation/MainActivity.kt
@@ -1,28 +1,86 @@
 package com.kpuc.showyourlocation
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.kpuc.showyourlocation.databinding.ActivityMainBinding
+import java.math.BigDecimal
+import java.math.RoundingMode
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
-
-    // inside a basic activity
-    private var locationManager: LocationManager? = null
     private lateinit var binding: ActivityMainBinding
+
+    private var locationManager: LocationManager? = null
+    private var receivedLocation: LatLng? = null
+    private var firstLoaded: Boolean = false
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        googleMap.clear()
+        googleMap.addMarker(receivedLocation?.let {
+            MarkerOptions().position(it)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                .alpha(0.5F)
+        })
+        if (!firstLoaded) {
+            followLocation()
+            firstLoaded = true
+        }
+    }
+
+    private val goToMyLocation = OnMapReadyCallback { googleMap ->
+        if (receivedLocation != null) {
+            val cameraPosition = CameraPosition.Builder()
+                .target(receivedLocation)
+                .zoom(15F).build()
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+        } else {
+            Toast.makeText(
+                applicationContext,
+                resources.getString(R.string.getting_location),
+                Toast.LENGTH_LONG
+            )
+                .show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val btnMyLocation = findViewById<Button>(R.id.btnMyLocation)
+        btnMyLocation.setOnClickListener {
+            followLocation()
+        }
         checkLocationPermissions()
+    }
+
+    private fun followLocation() {
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(goToMyLocation)
+    }
+
+    private fun updateMapLocation() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
 
     private fun startLocationUpdates() {
@@ -37,15 +95,27 @@ class MainActivity : AppCompatActivity() {
                 locationListener
             )
         } catch (ex: SecurityException) {
-            Log.d("myTag", "Security Exception, no location available")
+            Toast.makeText(
+                applicationContext,
+                resources.getString(R.string.location_exception),
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
     //define the listener
     private val locationListener: LocationListener = object : LocationListener {
+        @SuppressLint("SetTextI18n")
         override fun onLocationChanged(location: Location) {
-            binding.tvLocationLatitude.text = location.latitude.toString()
-            binding.tvLocationLongtitude.text = location.longitude.toString()
+            val speed = location.speed
+            val speedKmH = (speed * 3.6).roundToInt()
+            val longitude = BigDecimal(location.longitude).setScale(6, RoundingMode.HALF_EVEN)
+            val latitude = BigDecimal(location.latitude).setScale(6, RoundingMode.HALF_EVEN)
+            receivedLocation = LatLng(location.latitude, location.longitude)
+            binding.tvSpeed.text = speedKmH.toString() + resources.getString(R.string.speed)
+            binding.tvLocationLongitude.text = "x: $longitude"
+            binding.tvLocationLatitude.text = "y: $latitude"
+            updateMapLocation()
         }
 
         override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
@@ -58,7 +128,11 @@ class MainActivity : AppCompatActivity() {
             if (isGranted) {
                 startLocationUpdates()
             } else {
-                Log.i("MainActivity", "No Permission")
+                Toast.makeText(
+                    applicationContext,
+                    resources.getString(R.string.permission_denied),
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
 
@@ -66,17 +140,17 @@ class MainActivity : AppCompatActivity() {
         when {
             ContextCompat.checkSelfPermission(
                 this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
+                Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED -> {
                 startLocationUpdates()
             }
-            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)
             -> {
 
             }
             else -> {
                 requestPermissionLauncher.launch(
-                    Manifest.permission.ACCESS_COARSE_LOCATION
+                    Manifest.permission.ACCESS_FINE_LOCATION
                 )
             }
         }
