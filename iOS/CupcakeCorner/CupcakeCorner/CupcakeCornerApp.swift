16c20bb618d34873616e573bf959abc2fb59825f
Strahilov,Kristiyan K.D <k.strahilov@student.fontys.nl>
2021-04-22 01:18:40 +0200

    Implemented CupcakeCorner app

---
+++ iOS/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Kristiyan Strahilov on 21.04.21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
