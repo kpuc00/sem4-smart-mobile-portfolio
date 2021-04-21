4a13653e2e0d72309867b6d8f4820e706a94c02a
Kristiyan Strahilov <k.strahilov@student.fontys.nl>
2021-04-21 17:03:51 +0200

    Implemented WeSplit

--- iOS/WeSplit/WeSplit/ContentView.swift
+++ iOS/WeSplit/WeSplit/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = ""
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalAmount: Double {
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0 //?? means if the value is not valid return 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        
+        return grandTotal
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Int(numberOfPeople) ?? 1
+        let amountPerPerson = totalAmount / Double(peopleCount)
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.numberPad)
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Total amount")) {
+                    Text("$\(totalAmount, specifier: "%.2f")") // using specifier we change the number's decimal places
+                }
+                
+                Section(header: Text("Amount per person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")") // using specifier we change the number's decimal places
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
     }
 }
 
