0ae384c815e6e05863290c2639c78d813471b1fe
Strahilov,Kristiyan K.D <k.strahilov@student.fontys.nl>
2021-04-22 13:17:14 +0200

    Implemented GuessTheFlag

---
+++ iOS/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Kristiyan Strahilov on 22.04.21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
